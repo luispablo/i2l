b9e17c4cf2b143bc640c71279df17bda33b6f86b
Luis Pablo Gallo <luispablo.gallo@gmail.com>
2021-04-14 16:34:33 -0300

    Add to_lower option on each item

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-// TODO: -l toLower items
 // TODO: -q remove quotes
 
 const HELP_OPTION = ["h", "help"];
+const LOWER_OPTION = ["l", "lower"];
 const TRIM_OPTION = ["t", "trim"];
 
 const buildHasOption = function buildHasOption (argv) {
@@ -25,11 +25,14 @@ SYNOPSIS
 DESCRIPTION
     This tool takes the content from the clipboard and leaves the result also there, replacing the original content.
 
-    -t, --trim
-              Removes start and end spaces, turns " an_item  " into "an_item"
-
     -h, --help
               Print this help message
+
+    -l, --lower
+              Turn to lowercase each item
+
+    -t, --trim
+              Removes start and end spaces, turns " an_item  " into "an_item"
   `);
 };
 
@@ -45,7 +48,8 @@ DESCRIPTION
       const items = clipboardy.readSync().split("\n");
       const processedItems = items.reduce(function (prev, curr) {
         const trimmed = hasOption(TRIM_OPTION) ? curr.trim() : curr;
-        return prev.concat([trimmed]);
+        const lowered = hasOption(LOWER_OPTION) ? trimmed.toLowerCase() : trimmed;
+        return prev.concat([lowered]);
       }, []);
       const quotedItems = processedItems.map(i => `'${i}'`);
       const joinedQuotedItems = quotedItems.join(", ");
